8422778fc7040b2afd1f5c78a8d32353686cbbfb
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-10 18:42:09 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,22 +1,7 @@
-first_number =3
+first_number = 3
 second_number = 2
+
 sum=first_number+second_number
-
-puts sum
-
-first_number = 9
-second_number = 12
 difference=first_number-second_number
-
-puts difference
-
-first_number = 4
-second_number = 3
 product=first_number*second_number
-puts product
-
-first_number = 10
-second_number = 2
 quotient=first_number/second_number
-
-puts quotient
