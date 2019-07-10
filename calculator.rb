a11460a28c1eeaec9bea5fa4dc10fefa1f102b21
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-10 17:49:12 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,22 @@
-# Add your variables here
+first_number =3
+second_number = 2
+sum=first_number+second_number
+
+puts sum
+
+first_number = 9
+second_number = 12
+difference=first_number-second_number
+
+puts difference
+
+first_number = 4
+second_number = 3
+product=first_number*second_number
+puts product
+
+first_number = 10
+second_number = 2
+quotient=first_number/second_number
+
+puts quotient
