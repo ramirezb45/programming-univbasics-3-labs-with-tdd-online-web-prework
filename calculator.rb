abe0e6eb3ecf95d50503b8d7667ad902d632eedb
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-12 18:52:57 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 first_number = 3
 second_number = 2
 
@@ -5,3 +6,10 @@ sum=first_number+second_number
 difference=first_number-second_number
 product=first_number*second_number
 quotient=first_number/second_number
+=======
+first_number =(3)
+second_number = (2)
+sum=first_number+second_number
+
+puts sum
+>>>>>>> 90fd181906fbbc9842da8b035f1abc1d94e51efc
